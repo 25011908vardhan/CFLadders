d1ed7ae9a009c5d434a20c68237a9106521be60f
25011908vardhan <106120007@nitt.edu>
2022-06-26 23:56:00 +0530

    elegant solution, Implementation

--- B/49.cpp
+++ B/49.cpp
@@ -8,30 +8,13 @@ int main()
     int a[n];
     for(int i=0;i<n;i++)
     cin>>a[i];
-    int inc=1,dec=1;
+    int totalDecreasing=0,ind;
     for(int i=1;i<n;i++)
-    {
-        if(a[i]>a[i-1])dec=0;
-        else if(a[i]<a[i-1])inc=0;
+    if(a[i]<a[i-1]){totalDecreasing++;ind=i;}
+    if(totalDecreasing==0)cout<<0<<endl;
+    else if(totalDecreasing>1)cout<<-1<<endl;
+    else {
+        if(a[n-1]>a[0])cout<<-1<<endl;
+        else cout<<n-ind<<endl;
     }
-    if((inc==dec&&inc==1)||(inc==1))cout<<0<<endl;
-    else if(dec)(n==2)?cout<<1<<endl:cout<<-1<<endl;
-    else{
-        // cout<<"in"<<endl;
-        int i=1;
-        while(i<n&&a[i]>=a[i-1])
-        i++;
-        int k=i;
-        i++;
-        while(i<n&&a[i]>=a[i-1])
-        i++;
-        if(i!=n){cout<<-1<<endl;return 0;}
-        if(a[n-1]>a[0]){cout<<-1<<endl;return 0;}
-        i=1;
-        while(i<k&&a[i]>=a[i-1])
-        i++;
-        if(i!=k){cout<<-1<<endl;return 0;}
-        else cout<<n-k<<endl;
-    }
-    
 }
